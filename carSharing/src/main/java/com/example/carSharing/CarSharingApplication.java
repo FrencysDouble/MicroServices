9a6f9ac0fc6724500e8c36dbe79f7a69016570ff
Glebas <zgleb@bk.ru>
2024-11-29 08:12:27 +0300

    Adding loki+grafana+promtail for logging

--- carSharing/src/main/java/com/example/carSharing/CarSharingApplication.java
+++ carSharing/src/main/java/com/example/carSharing/CarSharingApplication.java
@@ -1,13 +1,19 @@
 package com.example.carSharing;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarSharingApplication {
+public class CarSharingApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarSharingApplication.class, args);
 	}
+
+
 
 }
